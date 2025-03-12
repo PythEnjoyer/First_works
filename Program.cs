e989bbeab32569b0f47148405cc027ac5163bdf1
PythEnjoyer <157293944+PythEnjoyer@users.noreply.github.com>
2025-03-12 23:04:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace My_first_program
+{
+
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            // Output of texe to the user
+            Console.WriteLine("I am starting to program in C#.");
+
+            // Waiting for Enter
+            Console.ReadLine();
+         }
+     }
+}
